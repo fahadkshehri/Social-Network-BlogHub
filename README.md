3bd8a4670aa2f69661005d1338d56aceff39f223
Fatih Ridha <abgfatih@gmail.com>
2018-12-03 13:05:56 -0800

    change readme and title

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Bloghub2
+# BlogHub
+
+A blogging application hosted on the cloud.
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.1.
 
@@ -25,3 +27,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Contributors
+
+- Abdulrahman Alajrami
+- Fahad Alshehri
+- Fatih Ridha
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bloghub2'`, () => {
+  it(`should have as title 'bloghub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bloghub2');
+    expect(app.title).toEqual("bloghub");
   });
 
-  it('should render title in a h1 tag', () => {
+  it("should render title in a h1 tag", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bloghub2!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to BlogHub!"
+    );
   });
 });
