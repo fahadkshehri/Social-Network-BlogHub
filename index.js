3135bd2c3e87d99fbe826b4cd454190794ae420d
Fahad <fahadkshehri@gmail.com>
2018-12-03 00:57:09 -0800

    Fixed Build Issue

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ mongoose.connect(config.uri, (err) => {
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
 app.use('/authentication', authentication); // Use Authentication routes in application
 app.use('/blogs', blogs); // Use Blog routes in application
 
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/src/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // Start Server: Listen on port 8080
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
 });
