029ed22d920cac9387c59de4858f5857c429c442
alajra <gxa.aj.911@gmail.com>
2018-12-04 23:28:09 -0800

    PHP files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Homepage</title>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/app.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  </head>
+  <body>
+
+    <div class="wrapper">
+
+    </div>
+
+  </body>
+</html>
