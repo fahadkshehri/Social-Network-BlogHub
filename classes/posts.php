a250eae0eba0bb6c4c44a41ce958926a4f534872
alajra <gxa.aj.911@gmail.com>
2018-12-05 22:39:01 -0800

    Posts updated

---
+++ classes/posts.php
@@ -0,0 +1,22 @@
+<?php
+
+class Posts{
+
+    public function getAllPost(){
+
+    }
+
+    public function addPost($title, $author, $text, $img){
+
+    }
+
+    public function removePost($postID){
+
+    }
+
+    public function editPost($id, $title, $img, $text){
+
+    }
+
+}
+?>
