9c6068d0dc6a1c4291e4efa19323f855f0578fe6
alajra <gxa.aj.911@gmail.com>
2018-12-06 19:40:11 -0800

    Posts updated

--- classes/posts.php
+++ classes/posts.php
@@ -1,24 +1,41 @@
 <?php
-
 require 'vendor/autoload.php';
 
-use Aws\DynamoDb\Exception\DynamoDbException;
-use Aws\DynamoDb\Marshaler;
-use Aws\Credentials\CredentialProvider;
-use Aws\S3\S3Client;
 
-date_default_timezone_set('UTC');
+$dsn = 'mysql:host=your_db_host;dbname=your_db_name;charset=utf8';
+$usr = 'your_db_username';
+$pwd = 'your_db_password';
 
-$sdk = new Aws\Sdk([
-    //'endpoint'   => 'http://localhost:8000',
-    'region'   => 'us-west-2',
-    'version'  => 'latest',
-]);
+$pdo = new \Slim\PDO\Database($dsn, $usr, $pwd);
 
-$dynamodb = $sdk->createDynamoDb();
-$marshaler = new Marshaler();
-$tableName = 'posts';
+// SELECT * FROM users WHERE id = ?
+$selectStatement = $pdo->select()
+                       ->from('users')
+                       ->where('id', '=', 1234);
 
+$stmt = $selectStatement->execute();
+$data = $stmt->fetch();
+
+// INSERT INTO users ( id , usr , pwd ) VALUES ( ? , ? , ? )
+$insertStatement = $pdo->insert(array('id', 'usr', 'pwd'))
+                       ->into('users')
+                       ->values(array(1234, 'your_username', 'your_password'));
+
+$insertId = $insertStatement->execute(false);
+
+// UPDATE users SET pwd = ? WHERE id = ?
+$updateStatement = $pdo->update(array('pwd' => 'your_new_password'))
+                       ->table('users')
+                       ->where('id', '=', 1234);
+
+$affectedRows = $updateStatement->execute();
+
+// DELETE FROM users WHERE id = ?
+$deleteStatement = $pdo->delete()
+                       ->from('users')
+                       ->where('id', '=', 1234);
+
+$affectedRows = $deleteStatement->execute();
 
 
 class Posts{
@@ -27,95 +44,16 @@ class Posts{
 
     }
 
-    posts->addPost("New to css","Fahad", "this is text", "pic.jpg");
-
-
     public function addPost($title, $author, $text, $img){
-
-        $item = $marshaler->marshalJson('
-            {
-                "title": "' . $title . '",
-                "author": "' . $author . '",
-                "text": "' . $text . '",
-                "img": "' . $img . '",
-            }
-        ');
-
-        $params = [
-            'TableName' => $tableName,
-            'Item' => $item
-        ];
-
-
-        try {
-            $result = $dynamodb->putItem($params);
-            echo "Added item \n";
-
-        } catch (DynamoDbException $e) {
-            echo "Unable to add item:\n";
-            echo $e->getMessage() . "\n";
-        }
 
     }
 
     public function removePost($postID){
-        $key = $marshaler->marshalJson('
-            {
-                "postID": "' . $postID . '"
-            }
-        ');
 
-
-        $params = [
-            'TableName' => $tableName,
-            'Key' => $key,
-        ];
-
-        try {
-            $result = $dynamodb->deleteItem($params);
-            echo "Deleted item.\n";
-
-        } catch (DynamoDbException $e) {
-            echo "Unable to delete item:\n";
-            echo $e->getMessage() . "\n";
-        }
     }
 
     public function editPost($id, $title, $img, $text){
-        $key = $marshaler->marshalJson('
-            {
-                "year": ' . $year . ',
-                "title": "' . $title . '"
-            }
-        ');
 
-
-        $eav = $marshaler->marshalJson('
-            {
-                ":t": "'.$title.'" ,
-                ":i": "'$img'",
-                ":t": "'$text'"
-            }
-        ');
-
-        $params = [
-            'TableName' => $tableName,
-            'Key' => $key,
-            'UpdateExpression' =>
-                'set info.rating = :r, info.plot=:p, info.actors=:a',
-            'ExpressionAttributeValues'=> $eav,
-            'ReturnValues' => 'UPDATED_NEW'
-        ];
-
-        try {
-            $result = $dynamodb->updateItem($params);
-            echo "Updated item.\n";
-            print_r($result['Attributes']);
-
-        } catch (DynamoDbException $e) {
-            echo "Unable to update item:\n";
-            echo $e->getMessage() . "\n";
-        }
     }
 
 }
