2196145ff7eb107022825564cd1d97dde046f744
Fatih Ridha <abgfatih@gmail.com>
2018-12-06 20:09:07 -0800

    database info

--- classes/posts.php
+++ classes/posts.php
@@ -1,60 +1,42 @@
 <?php
 require 'vendor/autoload.php';
 
-
-$dsn = 'mysql:host=your_db_host;dbname=your_db_name;charset=utf8';
-$usr = 'your_db_username';
-$pwd = 'your_db_password';
+$dsn = 'mysql:host=aa1cqahmu0x18pk.cisqnd3qf2b5.us-west-2.rds.amazonaws.com:3306;dbname=aa1cqahmu0x18pk;charset=utf8';
+$usr = 'BlogHubMaster';
+$pwd = 'abdulfahadfatih';
 
 $pdo = new \Slim\PDO\Database($dsn, $usr, $pwd);
 
 // SELECT * FROM users WHERE id = ?
 $selectStatement = $pdo->select()
-                       ->from('users')
-                       ->where('id', '=', 1234);
+    ->from('users');
 
 $stmt = $selectStatement->execute();
 $data = $stmt->fetch();
 
-// INSERT INTO users ( id , usr , pwd ) VALUES ( ? , ? , ? )
-$insertStatement = $pdo->insert(array('id', 'usr', 'pwd'))
-                       ->into('users')
-                       ->values(array(1234, 'your_username', 'your_password'));
+var_dump(data);
 
-$insertId = $insertStatement->execute(false);
+class Posts
+{
 
-// UPDATE users SET pwd = ? WHERE id = ?
-$updateStatement = $pdo->update(array('pwd' => 'your_new_password'))
-                       ->table('users')
-                       ->where('id', '=', 1234);
-
-$affectedRows = $updateStatement->execute();
-
-// DELETE FROM users WHERE id = ?
-$deleteStatement = $pdo->delete()
-                       ->from('users')
-                       ->where('id', '=', 1234);
-
-$affectedRows = $deleteStatement->execute();
-
-
-class Posts{
-
-    public function getAllPost(){
+    public function getAllPost()
+    {
 
     }
 
-    public function addPost($title, $author, $text, $img){
+    public function addPost($title, $author, $text, $img)
+    {
 
     }
 
-    public function removePost($postID){
+    public function removePost($postID)
+    {
 
     }
 
-    public function editPost($id, $title, $img, $text){
+    public function editPost($id, $title, $img, $text)
+    {
 
     }
 
 }
-?>
