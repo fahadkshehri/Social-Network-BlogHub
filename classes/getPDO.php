db1d14b7e09ff133c207678f8502d06838d2e503
Fatih Ridha <abgfatih@gmail.com>
2018-12-09 19:48:29 -0800

    update gitignore

--- classes/getPDO.php
+++ classes/getPDO.php
@@ -11,16 +11,12 @@ function getPDO()
     } else {
         $host = 'bloghub-rds.cisqnd3qf2b5.us-west-2.rds.amazonaws.com';
     }
-    error_log($user);
+
     $password = getenv('RDS_PASSWORD');
     if (!$password) {
         $password = 'rootroot';
     }
 
-    error_log($host);
-    error_log($user);
-    error_log($password);
-    error_log(getenv('RDS_DB_NAME'));
     $dsn = 'mysql:host='.$host.';dbname=bloghub;charset=utf8';
 
     return new \Slim\PDO\Database($dsn, $user, $password);
