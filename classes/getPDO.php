157527b32b3e76aef231a2d732ab510ca42b75a7
Fatih Ridha <abgfatih@gmail.com>
2018-12-09 18:09:46 -0800

    test pdo on ebs

--- classes/getPDO.php
+++ classes/getPDO.php
@@ -1,6 +1,11 @@
 <?
 function getPDO()
 {
+    error_log(getenv('RDS_HOSTNAME'));
+    error_log(getenv('RDS_PORT'));
+    error_log(getenv('RDS_DB_NAME'));
+    error_log(getenv('RDS_USERNAME'));
+    error_log(getenv('RDS_PASSWORD'));
     $dsn = 'mysql:host=localhost;dbname=bloghub;charset=utf8';
     $usr = 'root';
     $pwd = 'rootroot';
